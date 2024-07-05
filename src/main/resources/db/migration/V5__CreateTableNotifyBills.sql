d2dedd54ba982022e491bed430d407b3856c8d39
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2024-07-04 23:05:30 -0300

    refactor: text correction of exits to exists

--- src/main/resources/db/migration/V5__CreateTableNotifyBills.sql
+++ src/main/resources/db/migration/V5__CreateTableNotifyBills.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXITS NotifyBills (
+CREATE TABLE IF NOT EXISTS NotifyBills (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     description VARCHAR(255) NOT NULL,
     notification_date CHAR(2) NOT NULL,
