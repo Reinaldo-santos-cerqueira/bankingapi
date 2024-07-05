96e67b0e9b459c823d9befca270b2d77ffe45a5b
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2024-07-05 15:53:52 -0300

    feat: adding lombook for class UserModel

--- src/main/java/dev/reinaldosantos/bankingapi/domain/user/UserModel.java
+++ src/main/java/dev/reinaldosantos/bankingapi/domain/user/UserModel.java
@@ -2,10 +2,20 @@ package dev.reinaldosantos.bankingapi.domain.user;
 
 import java.util.UUID;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
 @Table(name="users")
 public class UserModel {
     @GeneratedValue(generator = "UUID")
@@ -13,62 +23,7 @@ public class UserModel {
     private UUID id;
     private String name;
     private String email;
-    private String password; 
+    private String password;
+    private Boolean active;
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        UserModel other = (UserModel) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    } 
 }
