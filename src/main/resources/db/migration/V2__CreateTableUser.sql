be02f8bc11ca20d6e94479167af38e684b90ceb7
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2024-07-08 00:09:13 -0300

    chore: add role for users

--- src/main/resources/db/migration/V2__CreateTableUser.sql
+++ src/main/resources/db/migration/V2__CreateTableUser.sql
@@ -1,8 +1,8 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    active BOOLEAN DEFAULT TRUE
+    active BOOLEAN DEFAULT TRUE,
+    role TEXT NOT NULL
 );
