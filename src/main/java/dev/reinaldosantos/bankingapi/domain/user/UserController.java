f62700e1ea155dc8a50980dc0f61c1c7a6798cdd
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2024-07-07 21:54:44 -0300

    feat: create controller to user

---
+++ src/main/java/dev/reinaldosantos/bankingapi/domain/user/UserController.java
@@ -0,0 +1,26 @@
+package dev.reinaldosantos.bankingapi.domain.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@RestController()
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+    @PostMapping("")
+    public ResponseEntity<User> create(@Valid @RequestBody UserDTO userDTO){
+        User createdUser = userService.create(userDTO);
+        return ResponseEntity.ok(createdUser);
+    }
+
+}
