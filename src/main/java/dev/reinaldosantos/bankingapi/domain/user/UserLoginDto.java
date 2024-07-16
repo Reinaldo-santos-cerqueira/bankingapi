0855fa541587702b5a800fec78f9401bac914387
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2024-07-15 23:25:02 -0300

    feat: create dto for a user with valids

---
+++ src/main/java/dev/reinaldosantos/bankingapi/domain/user/UserLoginDto.java
@@ -0,0 +1,23 @@
+package dev.reinaldosantos.bankingapi.domain.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserLoginDto {
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+    @NotBlank(message = "Password is mandatory")
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+        message = "Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character"
+    )
+    private String password;
+}
