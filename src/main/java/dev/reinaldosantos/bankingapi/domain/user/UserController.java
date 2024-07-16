9958a04f0859873beeef5ad705638cd2efd3eb9c
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2024-07-15 23:26:39 -0300

    feat: valid request login

--- src/main/java/dev/reinaldosantos/bankingapi/domain/user/UserController.java
+++ src/main/java/dev/reinaldosantos/bankingapi/domain/user/UserController.java
@@ -1,26 +1,41 @@
 package dev.reinaldosantos.bankingapi.domain.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-
+import lombok.RequiredArgsConstructor;
 
 @RestController()
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
-    public UserController(UserService userService){
-        this.userService = userService;
-    }
-    @PostMapping("")
-    public ResponseEntity<User> create(@Valid @RequestBody UserDTO userDTO){
+    @PostMapping("/register")
+    public ResponseEntity<User> create(@Valid @RequestBody UserDTO userDTO) {
         User createdUser = userService.create(userDTO);
         return ResponseEntity.ok(createdUser);
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, Object>> postMethodName(@Valid @RequestBody UserLoginDto userLoginDto) {
+        Map<String, Object> response = new HashMap<>();
+
+        String token = userService.login(userLoginDto);
+        if (token == "") {
+            response.put("message", "Password incorrect");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        } else {
+            response.put("token", token);
+            return ResponseEntity.ok(response);
+        }
+    }
 }
