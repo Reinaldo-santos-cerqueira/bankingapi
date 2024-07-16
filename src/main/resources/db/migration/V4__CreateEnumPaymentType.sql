f4452cbe07564b7d536dcd580cf67ec10e5b71ac
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2024-07-15 23:19:45 -0300

    feat: change config for transaction for fliway

--- src/main/resources/db/migration/V4__CreateEnumPaymentType.sql
+++ src/main/resources/db/migration/V4__CreateEnumPaymentType.sql
@@ -1,6 +1,6 @@
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'payment_types') THEN
-        CREATE TYPE payment_types AS ENUM ('pix', 'debit-card', 'credit-card', 'bank-slip', 'cash', 'transaction');
-    END IF;
-END $$;
+-- DO $$
+-- BEGIN
+--     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'payment_types') THEN
+--         CREATE TYPE payment_types AS ENUM ('pix', 'debit-card', 'credit-card', 'bank-slip', 'cash', 'transaction');
+--     END IF;
+-- END $$;
