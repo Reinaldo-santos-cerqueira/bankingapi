81608dac5c260578929d1c2daa502a62274f000f
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2024-07-15 23:25:45 -0300

    feat: implementation for a findByEmail

--- src/main/java/dev/reinaldosantos/bankingapi/domain/user/UserRepository.java
+++ src/main/java/dev/reinaldosantos/bankingapi/domain/user/UserRepository.java
@@ -1,8 +1,12 @@
 package dev.reinaldosantos.bankingapi.domain.user;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
 }
