db0bb4657ec6507e3531c16331c3b62b9cfd21f7
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2024-07-15 23:22:57 -0300

    refactor: change type id to uuid to string

--- src/main/java/dev/reinaldosantos/bankingapi/domain/user/User.java
+++ src/main/java/dev/reinaldosantos/bankingapi/domain/user/User.java
@@ -1,10 +1,14 @@
 package dev.reinaldosantos.bankingapi.domain.user;
 
 import java.util.Collection;
+import java.util.UUID;
+
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
@@ -18,23 +22,14 @@ import lombok.Setter;
 @EqualsAndHashCode
 @Entity
 @Table(name="users")
-public class User implements UserDetails {
-    @GeneratedValue(generator = "UUID")
+public class User {
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
     private String name;
     private String email;
     private String password;
     private Boolean active = true;
     private UserRole role;
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
-    }
-    @Override
-    public String getUsername() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
-    }
+
 }
