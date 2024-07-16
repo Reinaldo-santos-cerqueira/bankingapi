d6d43ea624592b7f529d65943a3dea6f8fb6349c
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2024-07-15 23:21:05 -0300

    feat: added custom detailsd for service to config core spring security

---
+++ src/main/java/dev/reinaldosantos/bankingapi/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package dev.reinaldosantos.bankingapi.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import dev.reinaldosantos.bankingapi.domain.user.User;
+import dev.reinaldosantos.bankingapi.domain.user.UserRepository;
+
+import java.util.ArrayList;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
