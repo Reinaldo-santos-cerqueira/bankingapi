bb12cd0edc9580c40502c1a31480f10a2216ce15
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2024-07-15 23:23:29 -0300

    feat: implementation create and valid token

--- src/main/java/dev/reinaldosantos/bankingapi/security/TokenService.java
+++ src/main/java/dev/reinaldosantos/bankingapi/security/TokenService.java
@@ -1,23 +1,54 @@
 package dev.reinaldosantos.bankingapi.security;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import dev.reinaldosantos.bankingapi.domain.user.User;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 @Service
 public class TokenService {
-    @Value("${api-security-token}")
+    @Value("${API_SECURITY_TOKEN_SECRET}")
     private String secret;
-    public String generateToken(User user){
+
+    public String generateToken(User user) {
         try {
-            Algorithm algorithm = Algorithm.HMAC256("");
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token = JWT.create()
+                    .withIssuer("login-auth-api")
+                    .withSubject(user.getEmail())
+                    .withSubject(user.getName())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
+            return token;
         } catch (JWTCreationException exception) {
-            throw new RuntimeException("Error while authenticating;");
+            throw new RuntimeException("Error while authenticating");
         }
-        return secret;
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("login-auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return null;
+        }
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 }
