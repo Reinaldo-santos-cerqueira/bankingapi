427b1105938a6b34d097460f1f088b36476ebff2
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2024-07-20 20:04:41 -0300

    refactor: adding supressWarnings

--- src/main/java/dev/reinaldosantos/bankingapi/domain/user/UserRepository.java
+++ src/main/java/dev/reinaldosantos/bankingapi/domain/user/UserRepository.java
@@ -9,5 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
+    @SuppressWarnings("null")
     Optional<User> findById(UUID id);
 }
