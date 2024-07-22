695d386a675e577d60c1a6a5476a0c3436e96d4e
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2024-07-21 22:47:08 -0300

    refactor: correct to annotations

--- src/main/java/dev/reinaldosantos/bankingapi/domain/user/UserDTO.java
+++ src/main/java/dev/reinaldosantos/bankingapi/domain/user/UserDTO.java
@@ -3,11 +3,10 @@ package dev.reinaldosantos.bankingapi.domain.user;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,22 +17,20 @@ import lombok.Setter;
 public class UserDTO {
 
     @NotBlank(message = "Name is mandatory")
-    @Min(5)
-    @Max(255)
+    @Size(min = 5, max = 255, message = "Name must be between 5 and 255 characters")
     private String name;
     @Email(message = "Email should be valid")
     @NotBlank(message = "Email is mandatory")
-    @Max(255)
+    @Size(max = 255, message = "Email must be at most 255 characters")
     private String email;
     @NotBlank(message = "Password is mandatory")
-    @Max(255)
+    @Size(max = 255, message = "Password must be at most 255 characters")
     @Pattern(
         regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
         message = "Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character"
     )
     private String password;
     @NotNull(message = "Role is mandatory")
-    @Pattern(regexp = "ADMIN|COMMON|ROLE_USER", message = "The user role must be ADMIN, COMMON ou ROLE_USER.")
     @Enumerated(EnumType.STRING)
     private UserRole role; 
 }
